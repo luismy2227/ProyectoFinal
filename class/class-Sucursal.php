438c1dcfefcb54ba65a0b84c4f703efa60994687
luismy2227 <luismy2227@gmail.com>
2018-04-28 15:31:36 -0600

    fotos

--- class/class-Sucursal.php
+++ class/class-Sucursal.php
@@ -1,6 +1,6 @@
 <?php
 
-	class class-Sucursal{
+	class Sucursal{
 
 		private $idSucursal;
 		private $descripcion;
@@ -35,6 +35,23 @@
 			return "IdSucursal: " . $this->idSucursal .
 				" Descripcion: " . $this->descripcion .
 				" IdDireccion: " . $this->idDireccion;
+		}
+
+		public static function listar($conexion){
+			$query = "SELECT tbl_VehiculoEmpresa.idVehiculoEmpresa idVehiculo, tbl_Marca.descripcion marca, tbl_Modelo.descripcion modelo,
+			tbl_VehiculoEmpresa.precioVenta precioVenta, tbl_VehiculoEmpresa.precioVenta precioRenta, tbl_Foto.rutaFoto foto FROM tbl_VehiculoEmpresa 
+			INNER JOIN tbl_Vehiculo ON tbl_Vehiculo.idVehiculo = tbl_VehiculoEmpresa.idVehiculo
+			INNER JOIN tbl_Marca ON tbl_Vehiculo.idMarca = tbl_Marca.idMarca
+			INNER JOIN tbl_Modelo ON tbl_Modelo.idModelo = tbl_Vehiculo.idModelo
+			INNER JOIN tbl_Foto ON tbl_Foto.idVehiculo = tbl_Vehiculo.idVehiculo;";
+			$vehiculos = $conexion -> ejecutarConsulta($query);
+			$carros = array();
+
+			while($respuesta=$conexion->obtenerFilas($vehiculos)){
+                $carros[]=$respuesta;
+            }
+            return $carros;
+
 		}
 	}
 ?>
---
+++ js/sucursales.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    //alert("hola1");
+    allCars();
+});
+
+function allCars(){
+    //alert("hola2");
+    $.ajax({
+        url:"ajax/gestionar-Sucursales.php",
+            dataType:"JSON",
+            method:"POST",
+            data:{
+                "accion":"listar"
+            },
+            success:function(respuesta){
+                //alert("hola3");
+                for (var i = 0; i < respuesta.length ; i++) {
+                    var carros = respuesta[i];
+                   var fila =  
+                                //'<div class="dotted_line"></div>'+
+                                '<div class ="Cajita" id="Cajita">'+
+                                '   <div class="col-md-6 col-lg-4" >'+
+                                '   <div class="single-feature">'+
+                                '       <div class="card">'+
+                                '           <div class="card-header cards-courses-h">'+carros.marca+" "+carros.modelo+
+                                '           </div>'+    
+                                '           <div class="card-body">'+
+                                '                   <h5 class="card-title">'+'</h5>'+
+                                /*'                   <p class="card-text">Precio de venta: </p>'+
+                                '                   <p class="card-text">'+carros.precioventa+'</p>'+*/
+                                '                   <img src="'+carros.foto+'" alt="" width="320" height="300">'+
+                                '                   <p><a class="btn btn-primary" href="#" role="button">Ver Vehículo &raquo;</a></p>'+
+                                '           </div>'+
+                                '       </div>'+
+                                '       </div>'+
+                                '   </div></div>';                    
+                    $("#carros").append(fila);
+                }
+            },
+            error:function(e){
+                console.log(e);
+            }
+    });
+}
